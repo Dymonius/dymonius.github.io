b6109d97175d1e424ff0d23dd388ee9cc73a0c50
Dmitry <dymonius12@gmail.com>
2017-09-21 14:32:41 +0300

    Sticky was added

--- StickyMenu/script.js
+++ StickyMenu/script.js
@@ -1,19 +1,18 @@
 var stickElement = document.querySelector(".menu");
 var fixed = false;
-var fixPoint = stickElement.offsetTop;
+var fixPoint = stickElement.offsetTop; //Получение расстояния по оси Y от верха страницы до элемента
 
 
 window.onscroll = function () {
-    var distance = stickElement.offsetTop - window.pageYOffset;
+    var distance = fixPoint - window.pageYOffset;
     var offset = window.pageYOffset; //Получение текущей прокрутки сверху
     if ((distance <= 0) && !fixed) {
-        stickElement.style.position = 'fixed';
-        stickElement.style.top = '0px';
-        stickElement.style.left = '0px';
+        stickElement.classList.add('menu--fixed');
 
         fixed = true;
     } else if (fixed && (offset <= fixPoint)) {
-        stickElement.style.position = 'static';
+        stickElement.classList.remove('menu--fixed');
+
         fixed = false;
     }
 };
