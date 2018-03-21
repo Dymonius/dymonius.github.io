8ee382dfdda21ad0d44d78a38f2059c52eab32ee
Dmitry <dymonius12@gmail.com>
2018-03-21 19:49:53 +0200

    MultiSelect was added

--- MultiSelect/script.js
+++ MultiSelect/script.js
@@ -1,61 +1,33 @@
-$(document).ready(function () {
-    function setLocation(curLoc){
-        try {
-            history.pushState(null, null, curLoc);
-            return;
-        } catch(e) {}
-        location.hash = '#' + curLoc;
+$(".dropdown dt a").on('click', function() {
+    $(".dropdown dd ul").slideToggle('fast');
+});
+
+$(".dropdown dd ul li a").on('click', function() {
+    $(".dropdown dd ul").hide();
+});
+
+function getSelectedValue(id) {
+    return $("#" + id).find("dt a span.value").html();
+}
+
+$(document).bind('click', function(e) {
+    var $clicked = $(e.target);
+    if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
+});
+
+$('.mutliSelect input[type="checkbox"]').on('click', function() {
+
+    var title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val(),
+        title = $(this).val() + ",";
+
+    if ($(this).is(':checked')) {
+        var html = '<span title="' + title + '">' + title + '</span>';
+        $('.multiSel').append(html);
+        $(".hida").hide();
+    } else {
+        $('span[title="' + title + '"]').remove();
+        var ret = $(".hida");
+        $('.dropdown dt a').append(ret);
+
     }
-
-
-    $('.counter').hide();
-
-    $(".dropdown dt a").on('click', function () {
-        $(".dropdown dd ul").slideToggle('fast');
-    });
-
-
-    function getSelectedValue(id) {
-        return $("#" + id).find("dt a span.value").html();
-    }
-
-    $(document).bind('click', function (e) {
-        var $clicked = $(e.target);
-        if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
-    });
-
-    var counter = 0;
-
-    $('.mutliSelect input[type="checkbox"]').on('click', function () {
-
-        var title = $(this).val();
-
-        if ($(this).is(':checked')) {
-            var html = '<span title="' + title + '">' + title + ",&nbsp" + '</span>';
-            $('.multiSel').append(html);
-            $(".hida").hide();
-            counter++;
-        } else {
-            $('span[title="' + title + '"]').remove();
-            var ret = $(".hida");
-            $('.dropdown dt a').append(ret);
-            counter--;
-        }
-        $('.counter').html(counter);
-        if(counter === 0){
-            $('.counter').hide();
-            $('.hida').show();
-            $('.multiSel').css('marginLeft', '0px');
-        }
-        else {
-            $('.counter').show();
-            $('.multiSel').css('marginLeft', '30px')
-        }
-        if(counter===1) {
-            setLocation(title);
-        }
-        else {
-            setLocation('1')
-        }
-    });
 });
